0b5708797be6e4dcb52738c6deb9e84842bb223c
Németh Péter <71811@4ig.hu>
2020-02-03 17:24:41 +0100

    mult hetiek, plusz sablon javitasa

--- src/components/summary.js
+++ src/components/summary.js
@@ -6,48 +6,85 @@ import ReactDOM from 'react-dom';
 // megoldás (inkább!); megszüntetés, megszűnés; oldás, oldódás (le~/fel~); oldat; kiszabadulás; kiszabadítás
 
 export class Summary extends React.Component {
-  render() {
-    return (
-	<div>
-		<div className='foreign-word'>
-			die Lösung, -en
-		</div>
-		<div className='meaning'>
-			<div className='random-light-color-1 meaning-group'>
-				<div className='meaning-group-content'>
-					<div className='close-meaning-group'>
-						<div className='close-meaning primary-meaning'>
-							megoldás
+
+	parseRawMeanings(meanings){
+		let meaningGroupDelimiter = ';'
+		let meaningGroups = meanings.split(meaningGroupDelimiter).forEach(e => e.trim()).filter(e => e!=='')
+		
+	}
+
+	render() {
+		return (
+		<div className='word-card'>
+			<div className='foreign-word'>
+				die Lösung, -en
+			</div>
+			<div className='meaning'>
+				<div className='random-light-color-1 meaning-group'>
+					<div className='meaning-group-content'>
+						<div className='close-meaning-group'>
+							<div className='close-meaning primary-meaning'>
+								megoldás
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className='random-light-color-2 meaning-group'>
+					<div className='meaning-group-content'>
+						<div className='close-meaning-group'>
+							<div className='close-meaning'>
+								megszüntetés
+							</div>
+						</div>
+						<div className='close-meaning-group'>
+							<div className='close-meaning'>
+								megszűnés
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className='random-light-color-3 meaning-group'>
+					<div className='meaning-group-content'>
+						<div className='close-meaning-group'>
+							<div className='close-meaning'>
+								oldás
+							</div>
+						</div>
+						<div className='close-meaning-group'>
+							<div className='close-meaning'>
+								feloldódás
+							</div>
+						</div>
+					</div>
+					<div className='meaning-group-level-comment'>
+						(le~/fel~)
+					</div>
+				</div>
+				<div className='random-light-color-4 meaning-group'>
+					<div className='meaning-group-content'>
+						<div className='close-meaning-group'>
+							<div className='close-meaning'>
+								oldat
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className='random-light-color-5 meaning-group'>
+					<div className='meaning-group-content'>
+						<div className='close-meaning-group'>
+							<div className='close-meaning'>
+								kiszabadulás
+							</div>
+						</div>
+						<div className='close-meaning-group'>
+							<div className='close-meaning'>
+								kiszabadítás
+							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-			<div className='random-light-color-2 meaning-group'>
-				<div className='meaning-group-content'>
-					<div className='close-meaning-group'><div className='close-meaning'>megszüntetés</div></div>
-					<div className='close-meaning-group'><div className='close-meaning'>megszűnés</div></div>
-				</div>
-			</div>
-			<div className='random-light-color-3 meaning-group'>
-				<div className='meaning-group-content'>
-					<div className='close-meaning-group'><div className='close-meaning'>oldás</div></div>
-					<div className='close-meaning-group'><div className='close-meaning'>feloldódás</div></div>
-				</div>
-				<div className='meaning-group-level-comment'>(le~/fel~)</div>
-			</div>
-			<div className='random-light-color-4 meaning-group'>
-				<div className='meaning-group-content'>
-					<div className='close-meaning-group'><div className='close-meaning'>oldat</div></div>
-				</div>
-			</div>
-			<div className='random-light-color-5 meaning-group'>
-				<div className='meaning-group-content'>
-					<div className='close-meaning-group'><div className='close-meaning'>kiszabadulás</div></div>
-					<div className='close-meaning-group'><div className='close-meaning'>kiszabadítás</div></div>
-				</div>
-			</div>
 		</div>
-	</div>
-    );
-  }
+		);
+	}
 }
